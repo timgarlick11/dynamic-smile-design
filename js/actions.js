32b52007c96b35cf95bf3bd2347dae86fbce08e8
Tim Garlick <timgarlick11@gmail.com>
2016-03-15 17:01:24 -0600

    added schema but messed up anchor tags

--- js/actions.js
+++ js/actions.js
@@ -1,9 +1,21 @@
-$(document).ready(function(){
 
+
+$(document).ready(function() {
 	console.log("hi");
-	$('.nav-icon').click(function() {
-		console.log("hey")
-		$('.mobile-nav-dropdown').slideToggle(500);
+	$("<i class='fa fa-caret-right'></i>").appendTo(".link-button").css('margin-left', '6px');
+	$("<i class='fa fa-caret-right'></i>").appendTo(".procedure-info").css('margin-left', '6px').css("color","#b59356");
 
+	$('.nav-icon').click(function() {
+		console.log("hey");
+		$('.mobile-nav-dropdown').slideToggle(500);
 	});
-})
+
+	$('.top-banner-scroll-button').click(function(){
+
+		var WH = $(window).height();  
+		var SH = $('body')[0].scrollHeight;
+		$('html, body').stop().animate({scrollTop: SH-WH}, 1000);
+ 		console.log( SH+' '+WH ); // TEST
+
+ 	}); 
+});
